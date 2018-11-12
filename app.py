9f2a55514be3d433abf8807871fcac39eb0d0b86
Dmytro Titov <dmytro.titov@gmail.com>
2018-11-12 11:30:45 +0100

    Receive FAIR attributes from TrackFind.

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ def index():
 @app.route('/togsuite', methods=['POST'])
 def to_gsuite():
     gsuite = GSuite()
-    for dataset in request.json:
+    attributes = request.json['attributes']
+    datasets = request.json['datasets']
+    for dataset in datasets:
         standard_content = dict((k, str(v)) for k, v in dataset['fair'].iteritems() if v)
         if 'uri' in standard_content:
             uri = standard_content.pop('uri', None)
