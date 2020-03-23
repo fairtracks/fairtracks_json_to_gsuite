0d1da753ff719b4f2f97c293f0de163fc69786c7
Radmila Kompova <radmilko@ifi.uio.no>
2020-03-23 10:38:34 +0100

    remove docinfo if present, add constants, add logging

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask, request
 from gsuite.GSuite import GSuite
 from gsuite.GSuiteComposer import composeToString
 from gsuite.GSuiteTrack import GSuiteTrack
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
@@ -12,6 +15,8 @@ ARRAY_SEP = ';'
 URL_PATH = 'file_url'
 TITLE_PATH = 'label_short'
 GENOME_PATH = 'assembly_name'
+DOC_INFO = 'doc_info'
+TRACKS = 'tracks'
 
 
 @app.route('/')
@@ -23,22 +28,30 @@ def index():
 def to_gsuite():
     gsuite = GSuite()
     data = json.loads(request.data, object_pairs_hook=OrderedDict)
+    app.logger.debug('Loaded JSON from request: ' + str(len(data)) + ' items')
     for item in data:
         createTracks(gsuite, item)
+    app.logger.debug('GSuite with ' + str(gsuite.numTracks()) + ' tracks created.')
+    strGsuite = composeToString(gsuite)
+    app.logger.debug('GSuite converted to string')
 
-    return composeToString(gsuite)
+    return strGsuite
 
 
 def createTracks(gsuite, data):
     if 'tracks' not in data:
+        app.logger.debug('No "tracks" element found!')
+        app.logger.debug(data)
         return
 
     trackData = OrderedDict()
-    for path, value in dictPaths(data['tracks']):
+    for path, value in dictPaths(data[TRACKS]):
         trackData[path] = value
 
     dataWithoutTracks = data.copy()
-    dataWithoutTracks.pop('tracks')
+    dataWithoutTracks.pop(TRACKS)
+    if DOC_INFO in dataWithoutTracks:
+        dataWithoutTracks.pop(DOC_INFO)
 
     noTrackData = OrderedDict()
     for path, value in dictPaths(dataWithoutTracks):
@@ -56,6 +69,8 @@ def createTracks(gsuite, data):
 
     uri = resultOrdered.pop(URL_PATH, None)
     if not uri:
+        app.logger.debug('Could not find uri in the element!')
+        app.logger.debug(resultOrdered)
         return
     gsuite.addTrack(GSuiteTrack(uri=uri, attributes=resultOrdered, title=resultOrdered[TITLE_PATH],
                                 genome=resultOrdered[GENOME_PATH]))
@@ -69,7 +84,7 @@ def dictPaths(myDict, path=[]):
                 yield item
         else:
             # track attributes should not have 'tracks->' in the attribute name
-            if newPath[0] == 'tracks':
+            if newPath[0] == TRACKS:
                 yield SEP.join(newPath[1:]), str(v)
             else:
                 if isinstance(v, list):
